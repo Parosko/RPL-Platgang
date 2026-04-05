985333cace99df12930aaae1e293d8cef61abb8b
Pariski <seigan4321@gmail.com>
2026-04-05 19:28:27 +0700

    STYLING: Color revised

--- views/admin/dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../core/middleware.php';
-onlyAdmin();
-?>
-
-<h2>Dashboard Admin</h2>
-<a href="../../controllers/auth/logout.php">Logout</a>
--- views/dpa/dashboard.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../core/middleware.php';
-onlyDPA();
-?>
-
-<h2>Dashboard DPA</h2>
-<a href="../../controllers/auth/logout.php">Logout</a>
